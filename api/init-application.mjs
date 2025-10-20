57260fa8b615c7e4340565a2513bdc45cdc9f095
sami23ibrahim <sami23ibrahim@gmail.com>
2025-10-20 15:39:19 +0200

    job 3

--- api/init-application.mjs
+++ api/init-application.mjs
@@ -2,9 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
-  { auth: { persistSession: false } }
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 const BUCKET = process.env.SUPABASE_BUCKET || 'cvs';
